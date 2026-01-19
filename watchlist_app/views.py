f57466d61efe03a98554a0303b6be436c93030fe
Matheus <mtzcooler@gmail.com>
2026-01-19 15:59:40 -0300

    Movie list and details using Django

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from watchlist_app.models import Movie
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.filter(active=True)
+    data = {
+        'movies': list(movies.values()),
+    }
+    return JsonResponse(data)
+
+def movie_detail(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk, active=True)
+        data = {
+            'title': movie.title,
+            'description': movie.description,
+            'active': movie.active,
+        }
+        return JsonResponse(data)
+    except Movie.DoesNotExist:
+        return JsonResponse({'error': 'Movie not found'}, status=404)
