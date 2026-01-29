aae8bd6ceae3dda00d61b46a58a55cd8241c0d26
Matheus <mtzcooler@gmail.com>
2026-01-29 17:00:50 -0300

    Serializers and api view

---
+++ watchlist_app/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def movie_detail(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({'error': 'Movie not found'}, status=404)
+
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
